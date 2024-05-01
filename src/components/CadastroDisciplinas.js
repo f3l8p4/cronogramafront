4356e500f5c4cd0e2c0ec2399650332e5a992613
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2024-05-01 00:49:48 -0300

    Update CadastroDisciplinas.js

--- src/components/CadastroDisciplinas.js
+++ src/components/CadastroDisciplinas.js
@@ -1,11 +1,22 @@
-import React  from "react";
+import React,{useState} from "react";
+import { useForm } from 'react-hook-form';
 
 const CadDisciplina = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
     
+    const onSubmit = (data) => {
+        console.log(data)
+       };
     return(
         <div>
             <h1>Cadastro de disciplinas</h1>
-            
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="nomeDisciplina">Nome: </label>
+                    <input type="text" id="nomeDisciplina" {...register('name', { required: 'Por favor, insira um nome Para a disciplina'})}/>
+                    {errors.nomeDisciplina && <div>{errors.nomeDisciplina.message}</div>}
+                </div>
+            </form>
         </div>
     )
 }
