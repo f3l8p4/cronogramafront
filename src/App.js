65f177f9327a1357ccd508dbfb66bfb7e506b3e3
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2024-05-24 23:19:35 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { useForm } from 'react-hook-form';
 import React, { useState } from 'react';
-import CadProfessor from './components/CadastroProfessores';
-import Routes from './Routes/Routes';
-import CadCurso from './components/Curso';
-import CadDisciplina from './components/CadastroDisciplinas';
-import CadCoordenador from './components/Coordenador';
-import CadProfessor2 from './components/CadProfessor2';
 import Login from './pages/Login';
+import CadProfessor from './components/Cadastros/CadastroProfessores';
+import ListaProfessores from './components/views/ListaProfessores';
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <ListaProfessores/>
     </div>
   );
 }
