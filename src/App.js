89ae333f75637166fee8b1d9e00533071a2a8ef7
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2024-05-28 00:00:05 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CadProfessor from './components/Cadastros/CadastroProfessores';
 import ListaProfessores from './components/views/ListaProfessores';
 import ListaCoordenadores from './components/views/ListaCoordenador';
+import CadCoordenador from './components/Cadastros/Coordenador';
 function App() {
   return (
     <div className="App">
@@ -16,6 +17,7 @@ function App() {
         <Route path='/coordenadores' element={<ListaCoordenadores/>}/>
         <Route path="/cadastro/" element={<CadProfessor />} />
         <Route path="/editarProfessor/:id" element={<CadProfessor />} />
+        <Route path="/editarCoordenador/:id" element={<CadCoordenador />} />
       </Routes>
     </Router>
     </div>
