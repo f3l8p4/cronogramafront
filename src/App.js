fa87e255cba1c06c612a1489b21269a07a01a243
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2024-05-27 22:25:06 -0300

    funcionamento das rotas

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import './App.css';
 import { useForm } from 'react-hook-form';
 import React, { useState } from 'react';
 import Login from './pages/Login';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CadProfessor from './components/Cadastros/CadastroProfessores';
 import ListaProfessores from './components/views/ListaProfessores';
 function App() {
   return (
     <div className="App">
-          <Router>
-      <Switch>
-        <Route exact path="/" component={ListaProfessores} />
-        <Route path="/editar/:id" component={CadProfessor} />
-        <Route path="/excluir/:id" component={ExcluirProfessor} />
-      </Switch>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ListaProfessores />} />
+        <Route path="/editar/:id" element={<CadProfessor />} />
+      </Routes>
     </Router>
     </div>
   );
