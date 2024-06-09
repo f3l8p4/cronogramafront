19dbc026474504081fe13cfd8aa3ebee9ae7abc2
Felipe Oliveira de Souza <felipe210603@hotmail.com>
2024-06-09 15:43:47 -0300

    organização das rotas

--- src/App.js
+++ src/App.js
@@ -3,43 +3,13 @@ import './App.css';
 import { useForm } from 'react-hook-form';
 import React, { useState } from 'react';
 import Login from './pages/Login';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CadProfessor from './components/Cadastros/CadastroProfessores';
-import ListaProfessores from './components/views/ListaProfessores';
-import ListaCoordenadores from './components/views/ListaCoordenador';
-import CadCoordenador from './components/Cadastros/Coordenador';
-import ListaDisciplinas from './components/views/ListaDisciplinas';
-import CadDisciplina from './components/Cadastros/CadastroDisciplinas';
-import ListaFases from './components/views/ListaFases';
-import CadFase from './components/Cadastros/CadastroFase';
-import CadCurso from './components/Cadastros/cadCurso'
-import ListaCurso from './components/views/ListaCurso';
-import ListaAgendaProfessor from './components/views/ListaAgendaProfessor';
-import CadAgendaProfessor from './components/Cadastros/CadastroAgendaProfessor';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routers from './Routes/Routes';
 function App() {
   return (
     <div className="App">
     <Router>
-      <Routes>
-        <Route path="/" element={<ListaProfessores />} />
-        <Route path='/coordenadores' element={<ListaCoordenadores/>}/>
-        <Route path='/disciplinas' element={<ListaDisciplinas/>} />
-        <Route path='/fases' element={<ListaFases/>}/>
-        <Route path='/cursos' element = {<ListaCurso/>}/>
-        <Route path='/agendaprofessores' element = {<ListaAgendaProfessor/>}/>
-        <Route path="/cadastro/" element={<CadProfessor />} />
-        <Route path="/editarProfessor/:id" element={<CadProfessor />} />
-        <Route path='/cadastroCoordenador' element={<CadCoordenador/>}/>
-        <Route path="/editarCoordenador/:id" element={<CadCoordenador />} />
-        <Route path='/cadastroDisciplina/' element={<CadDisciplina/>} />
-        <Route path='/cadastroCurso/' element={<CadCurso/>} />
-        <Route path='/cadastroAgendaProfessor/' element={<CadAgendaProfessor/>} />
-        <Route path='/editarDisciplina/:id' element={<CadDisciplina/>} />
-        <Route path='/cadastroFase' element={<CadFase/>} />
-        <Route path='/editarFase/:id' element={<CadFase/>} />
-        <Route path='/editarCurso/:id' element={<CadCurso/>} />
-        <Route path='/editarAgendaProfessor/:id' element={<CadAgendaProfessor/>} />
-      </Routes>
+      <Routers/>
     </Router>
     </div>
   );
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import { Router,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import ListaProfessores from '../components/views/ListaProfessores';
+import ListaCoordenadores from '../components/views/ListaCoordenador';
+import ListaDisciplinas from '../components/views/ListaDisciplinas';
+import ListaFases from '../components/views/ListaFases';
+import ListaCurso from '../components/views/ListaCurso';
+import CadDiaExcecao from '../components/Cadastros/CadastroDiaDeExcecao';
+import ListaAgendaProfessor from '../components/views/ListaAgendaProfessor';
+import CadProfessor from '../components/Cadastros/CadastroProfessores';
+import CadCoordenador from '../components/Cadastros/Coordenador';
+import CadDisciplina from '../components/Cadastros/CadastroDisciplinas';
+import CadCurso from '../components/Cadastros/cadCurso';
+import CadAgendaProfessor from '../components/Cadastros/CadastroAgendaProfessor';
+import CadFase from '../components/Cadastros/CadastroFase';
 
-import CadDisciplina from '../components/CadastroDisciplinas';
-import CadProfessor from '../components/CadastroProfessores';
-
-
-const Routes = () => {
+const Routers = () => {
   return (
-    <Router>
-            <Route path='/disciplina' component={<CadDisciplina/>} />
-            <Route path='/professor' componet={<CadProfessor/>} />
-    </Router>
+    <Routes>
+      <Route path="/" element={<ListaProfessores />} />
+      <Route path='/coordenadores' element={<ListaCoordenadores/>}/>
+      <Route path='/disciplinas' element={<ListaDisciplinas/>} />
+      <Route path='/fases' element={<ListaFases/>}/>
+      <Route path='/cursos' element = {<ListaCurso/>}/>
+      <Route path='/diaExcecao' element = {<CadDiaExcecao/>}/>
+      <Route path='/agendaprofessores' element = {<ListaAgendaProfessor/>}/>
+      <Route path="/cadastro/" element={<CadProfessor />} />
+      <Route path="/editarProfessor/:id" element={<CadProfessor />} />
+      <Route path='/cadastroCoordenador' element={<CadCoordenador/>}/>
+      <Route path="/editarCoordenador/:id" element={<CadCoordenador />} />
+      <Route path='/cadastroDisciplina/' element={<CadDisciplina/>} />
+      <Route path='/cadastroCurso/' element={<CadCurso/>} />
+      <Route path='/cadastroAgendaProfessor/' element={<CadAgendaProfessor/>} />
+      <Route path='/editarDisciplina/:id' element={<CadDisciplina/>} />
+      <Route path='/cadastroFase' element={<CadFase/>} />
+      <Route path='/editarFase/:id' element={<CadFase/>} />
+      <Route path='/editarCurso/:id' element={<CadCurso/>} />
+      <Route path='/editarAgendaProfessor/:id' element={<CadAgendaProfessor/>} />
+    </Routes>
   );
-}
+};
 
-export default Routes;
+export default Routers;
